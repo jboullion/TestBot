a60b93a169cb0855915073f14ab14a21ca113947
jboullion <jboullion83@gmail.com>
2021-09-01 20:52:56 -0500

    Initial Commit

---
+++ src/bot.js
@@ -0,0 +1,13 @@
+import { Client, Intents } from "discord.js";
+
+const bot = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+bot.on("ready", () => console.log(bot.user.username + " is online"));
+bot.on("message", async (msg) => {
+  // Don't reply to bots
+  if (msg.author.bot) return;
+
+  await msg.reply("Hi");
+});
+
+bot.login("BOT_TOKEN");
