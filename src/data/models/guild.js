0dd4a24350cfa5a75c4591b918e3afb4520ce82b
jboullion <jboullion83@gmail.com>
2021-09-02 22:45:32 -0500

    Setup mongoDB and add a prefix command

---
+++ src/data/models/guild.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+export const SavedGuild = mongoose.model(
+  "guild",
+  new mongoose.Schema({
+    _id: String,
+    prefix: { type: String, default: "." },
+  })
+);
