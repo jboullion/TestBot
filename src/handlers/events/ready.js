69842bd4b580c79e9bb0ec6df6d70eb087996050
jboullion <jboullion83@gmail.com>
2021-09-05 11:28:19 -0500

    Collecting all messages in a channel

--- src/handlers/events/ready.js
+++ src/handlers/events/ready.js
@@ -5,6 +5,8 @@ import Deps from "../../utils/deps.js";
 // import { MessageEmbed } from "discord.js";
 // import { Tags } from "../../data/models/tag.js";
 import { ChannelMessages } from "../../data/models/channel-messages.js";
+import moment from "moment";
+
 
 // const embed = new MessageEmbed()
 // 	.setTitle('Some Title')
@@ -23,6 +25,25 @@ export default class extends Event {
     bot.user.setActivity('Everything', { type: 'WATCHING'});
 
     ChannelMessages.sync({ force: true });
+
+    const channel = bot.channels.cache.get("866066557331570710");
+    channel.messages.fetch().then(async messages => {
+      console.log(`${messages.size} messages.`);
+
+      let finalArray = [];
+  
+      const putInArray = async (data) => finalArray.push(data);
+      const handleTime = (timestamp) => moment(timestamp).format("YYYYMMDD - hh:mm:ss a").replace("pm", "PM").replace("am", "AM"); 
+  
+      for (const message of messages.array().reverse()) {
+        if (!message.guild || message.author.bot) continue;
+
+        await putInArray(`${handleTime(message.createdTimestamp)} ${message.author.username} : ${message.content}`);
+      }
+  
+      console.log(finalArray);
+      console.log(finalArray.length);
+    })
 
     // const channel = bot.channels.cache.get('866066557331570708');
     // try {
